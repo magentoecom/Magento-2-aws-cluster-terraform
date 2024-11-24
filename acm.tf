adb74d5e0625ed72ce19bfe1266aee27b57b89d8
MagenX <magenx@users.noreply.github.com>
2024-11-24 12:23:56 +0100

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -7,15 +7,30 @@
 # Create ssl certificate for domain and subdomains
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_acm_certificate" "default" {
-  domain_name               = "${var.app["domain"]}"
-  subject_alternative_names = ["*.${var.app["domain"]}"]
+  domain_name               = "${var.domain}"
+  subject_alternative_names = ["*.${var.domain}"]
   validation_method         = "EMAIL"
 
-lifecycle {
+  lifecycle {
     create_before_destroy   = true
+  }
+  tags = {
+    Name = "${local.project}-${var.domain}-cert"
+  }
 }
-tags = {
-    Name = "${local.project}-${var.app["domain"]}-cert"
+
+resource "aws_acm_certificate" "cloudfront" {
+  count                     = data.aws_region.current.name == "us-east-1" ? 0 : 1
+  provider                  = aws.useast1
+  domain_name               = "${var.domain}"
+  subject_alternative_names = ["*.${var.domain}"]
+  validation_method         = "EMAIL"
+
+  lifecycle {
+    create_before_destroy   = true
+  }
+  tags = {
+    Name = "${local.project}-${var.domain}-cert"
   }
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
@@ -25,4 +40,8 @@ resource "aws_acm_certificate_validation" "default" {
   certificate_arn = aws_acm_certificate.default.arn
 }
 
-
+resource "aws_acm_certificate_validation" "cloudfront" {
+  count           = data.aws_region.current.name == "us-east-1" ? 0 : 1
+  provider        = aws.useast1
+  certificate_arn = aws_acm_certificate.cloudfront[0].arn
+}
