2fc89e0d12569995c9a5388d9314d7b97ddaeb90
MagenX <magenx@users.noreply.github.com>
2024-11-24 12:19:53 +0100

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -7,7 +7,7 @@
 # Create Lambda IAM role and attach policy permissions
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_cloudwatch_log_group" "lambda" {
-  name              = "/lambda/${aws_lambda_function.image_optimization.last_modified}"
+  name              = "/lambda/${aws_lambda_function.image_optimization.function_name}/"
   retention_in_days = 7
 }
 
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "lambda" {
       "logs:CreateLogStream",
       "logs:PutLogEvents",
     ]
-    resources = [aws_cloudwatch_log_group.lambda.arn]
+    resources = ["*"]
   }
 
   statement {
@@ -66,47 +66,78 @@ resource "aws_iam_role" "lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
+# Create Lambda permissions for CloudFront
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lambda_permission" "this" {
+  provider      = aws.useast1
+  statement_id  = "AllowCloudFrontServicePrincipal"
+  action        = "lambda:InvokeFunctionUrl"
+  function_name = aws_lambda_function.image_optimization.function_name
+  principal     = "cloudfront.amazonaws.com"
+  source_arn    = aws_cloudfront_distribution.this.arn
+  qualifier     = aws_lambda_alias.image_optimization.name
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create Lambda function npm package
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "null_resource" "npm_install" {
+  provisioner "local-exec" {
+    command = "cd ${abspath(path.root)}/lambda/image_optimization && npm install"
+  }
+  triggers = {
+    always_run = "${filesha256("${abspath(path.root)}/lambda/image_optimization/index.mjs")}"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
 # Create Lambda function zip archive 
 # # ---------------------------------------------------------------------------------------------------------------------#
 data "archive_file" "lambda_image_optimization" {
+  depends_on       = [null_resource.npm_install]
   type             = "zip"
-  source_file      = "${abspath(path.root)}/lambda/image_optimization/index.mjs"
+  source_dir       = "${abspath(path.root)}/lambda/image_optimization"
   output_file_mode = "0666"
-  output_path      = "${abspath(path.root)}/lambda/image_optimization/index.mjs.zip"
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Upload Lambda function zip archive to s3 bucket
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_s3_object" "lambda_image_optimization" {
-  depends_on = [data.archive_file.lambda_image_optimization]
-  bucket     = aws_s3_bucket.this["system"].id
-  key        = "lambda/image_optimization/index.js.zip"
-  source     = data.archive_file.lambda_image_optimization.output_path
-  etag       = filemd5(data.archive_file.lambda_image_optimization.output_path)
+  output_path      = "${abspath(path.root)}/lambda/image_optimization.zip"
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Lambda function with variables
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_lambda_function" "image_optimization" {
-  depends_on    = [aws_s3_object.lambda_image_optimization]
+  provider      = aws.useast1
   function_name = "${local.project}-image-optimization"
   role          = aws_iam_role.lambda.arn
-  s3_bucket     = aws_s3_bucket.this["system"].id
-  s3_key        = aws_s3_object.lambda_image_optimization.key
-  runtime       = "nodejs18.x"
+  filename      = data.archive_file.lambda_image_optimization.output_path
+  source_code_hash = data.archive_file.lambda_image_optimization.output_base64sha256
+  runtime       = "nodejs20.x"
   handler       = "index.handler"
-  memory_size   = 1500
-  timeout       = 60
+  memory_size   = 256
+  timeout       = 30
+  publish       = true
   environment {
     variables = {
+      s3BucketRegion             = aws_s3_bucket.this["media"].region
       originalImageBucketName    = aws_s3_bucket.this["media"].id
       transformedImageBucketName = aws_s3_bucket.this["media-optimized"].id
       transformedImageCacheTTL   = "max-age=31622400"
       maxImageSize               = "4700000"
    }
   }
-  vpc_config {
-    subnet_ids = values(aws_subnet.this).*.id 
-    security_group_ids = [aws_security_group.lambda.id]
-  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Lambda function url
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lambda_function_url" "image_optimization" {
+  provider           = aws.useast1
+  function_name      = aws_lambda_function.image_optimization.function_name
+  qualifier          = aws_lambda_alias.image_optimization.name
+  authorization_type = "AWS_IAM"
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Lambda function alias
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lambda_alias" "image_optimization" {
+  provider         = aws.useast1
+  name             = "${local.project}-image-optimization"
+  description      = "Lambda image optimization alias for ${local.project}"
+  function_name    = aws_lambda_function.image_optimization.arn
+  function_version = "$LATEST"
 }
