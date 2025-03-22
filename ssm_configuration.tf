22f0a74d2bb4f8fffdd480a7fa26705de6d4c2d3
MagenX <magenx@users.noreply.github.com>
2025-03-22 18:22:10 +0100

    Update ssm_configuration.tf

--- ssm_configuration.tf
+++ ssm_configuration.tf
@@ -12,77 +12,77 @@ resource "aws_ssm_document" "configuration" {
   document_type   = "Automation"
   content = <<EOF
 schemaVersion: "0.3"
-description: "Instance configuration step"
+description: "Runs Ansible Playbook to configure EC2 instance"
+assumeRole: ${aws_iam_role.ssm_service_role.arn}
 parameters:
-  EventSource:
+  S3ObjectKey:
     type: String
-    description: "Event Source"
-    default: ""
-  Force:
-    type: String
-    description: "Force SSM Document Steps Execution"
-    default: "false"
-  LogFileName:
-    type: String
-    description: "SSM Document Execution log file"
-    default: "/tmp/ssm_execution_log.txt"
+    description: "The S3 object key path"
 mainSteps:
-  - name: "InstanceConfiguration"
-    action: "aws:runCommand"
+  - name: "ExtractInstanceName"
+    action: aws:executeScript
+    inputs:
+      Runtime: python3.11
+      Handler: extract_instance_name
+      Script: |-
+        import re
+        def extract_instance_name(event, context):
+            s3_object_key = event['S3ObjectKey']
+            instance_name = s3_object_key.split('/')[1]
+            return { "InstanceName": instance_name }
+      InputPayload:
+        S3ObjectKey: "{{ S3ObjectKey }}"
+    outputs:
+      - Name: InstanceName
+        Selector: "$.Payload.InstanceName"
+        Type: String
+  - name: "FilterInstancesByNameTag"
+    action: aws:executeAwsApi
+    inputs:
+      Service: ec2
+      Api: DescribeInstances
+      Filters:
+        - Name: tag:InstanceName
+          Values:
+            - "{{ ExtractInstanceName.InstanceName }}"
+    outputs:
+      - Name: InstanceIds
+        Selector: "$.Reservations..Instances..InstanceId"
+        Type: StringList
+  - name: "RunCommandOnInstances"
+    action: aws:runCommand
     inputs:
       DocumentName: "AWS-RunShellScript"
+      InstanceIds: "{{ FilterInstancesByNameTag.InstanceIds }}"
       Parameters:
         commands:
           - |-
             #!/bin/bash
-            echo "Start configuration step $(date)" >> {{ LogFileName }}
-            INSTANCE_NAME=$(metadata tags/instance/Instance_name)
+            sudo pipx install ansible-core
+            sudo /root/.local/bin/ansible localhost -m ping > /dev/null 2>&1 && echo "SUCCESS: Ansible ping worked!" || { echo "ERROR: Ansible ping failed!"; exit 1; }
+            INSTANCE_NAME=$(metadata tags/instance/InstanceName)
+            INSTANCE_IP=$(metadata local-ipv4)
             SETUP_DIRECTORY="/opt/${var.brand}/setup"
-            LOG_DIRECTORY="$${SETUP_DIRECTORY}/log"
-            HASH_DIRECTORY="$${SETUP_DIRECTORY}/.hash"
-            INIT_DIRECTORY="$${SETUP_DIRECTORY}/instance"
             INSTANCE_DIRECTORY="$${SETUP_DIRECTORY}/$${INSTANCE_NAME}"
-            mkdir -p "$${LOG_DIRECTORY}"
-            mkdir -p "$${HASH_DIRECTORY}"
-            mkdir -p "$${INIT_DIRECTORY}"
             mkdir -p "$${INSTANCE_DIRECTORY}"
             touch $${SETUP_DIRECTORY}/init
-            OPTIONS="--quiet --exact-timestamps --delete"
-            aws s3 sync "s3://${aws_s3_bucket.this["system"].bucket}/setup/instance" "$${INIT_DIRECTORY}" $${OPTIONS} && \
-            aws s3 sync "s3://${aws_s3_bucket.this["system"].bucket}/setup/$${INSTANCE_NAME}" "$${INSTANCE_DIRECTORY}" $${OPTIONS}
-            if [ $? -eq 0 ]; then
-                echo "-- Configuration file:" >> {{ LogFileName }}
-                for SCRIPT in $(ls "$${INIT_DIRECTORY}"/*.sh | sort); do
-                    LOG_FILE="$${LOG_DIRECTORY}/$(basename "$${SCRIPT}").log"
-                    HASH_FILE="$${HASH_DIRECTORY}/$(basename "$${SCRIPT}").md5sum"
-                    NEW_HASH=$(md5sum "$${SCRIPT}" | awk '{print $1}')        
-                    if [ ! -f "$${HASH_FILE}" ] || [ "$${NEW_HASH}" != "$(cat "$${HASH_FILE}")" ]; then
-                        echo "$${NEW_HASH}" > "$${HASH_FILE}"
-                        echo -e "\n$(date)\nRunning: $${SCRIPT}" | tee -a "$${LOG_FILE}"
-                        bash "$${SCRIPT}" >>"$${LOG_FILE}" 2>&1
-                        echo "---- $${SCRIPT}" >> {{ LogFileName }}
-                    fi
-                done
-                for SCRIPT in $(ls "$${INSTANCE_DIRECTORY}"/*.sh | sort); do
-                    LOG_FILE="$${LOG_DIRECTORY}/$(basename "$${SCRIPT}").log"
-                    HASH_FILE="$${HASH_DIRECTORY}/$(basename "$${SCRIPT}").md5sum"
-                    NEW_HASH=$(md5sum "$${SCRIPT}" | awk '{print $1}')        
-                    if [ ! -f "$${HASH_FILE}" ] || [ "$${NEW_HASH}" != "$(cat "$${HASH_FILE}")" ]; then
-                        echo "$${NEW_HASH}" > "$${HASH_FILE}"
-                        echo -e "\n$(date)\nRunning: $${SCRIPT}" | tee -a "$${LOG_FILE}"
-                        bash "$${SCRIPT}" >>"$${LOG_FILE}" 2>&1
-                        echo "---- $${SCRIPT}" >> {{ LogFileName }}
-                    fi
-                done
-            else
-                echo "-- [ERROR]: Configuration files not found" >> {{ LogFileName }}
-            fi
+            S3_OPTIONS="--quiet --exact-timestamps --delete"
+            sudo /root/awscli/bin/aws s3 sync "s3://${aws_s3_bucket.this["system"].bucket}/setup/$${INSTANCE_NAME}" "$${INSTANCE_DIRECTORY}" $${S3_OPTIONS}
+            find $${INSTANCE_DIRECTORY}/ -type f -name '*.y*ml' -delete
+            cd $${INSTANCE_DIRECTORY}/
+            unzip -o $${INSTANCE_NAME}.zip
+            sudo /root/.local/bin/ansible-playbook -i localhost -c local -e "SSM=True instance_name=$${INSTANCE_NAME} instance_ip=$${INSTANCE_IP}" -v  $${INSTANCE_DIRECTORY}/$${INSTANCE_NAME}.yml
+      CloudWatchOutputConfig:
+        CloudWatchLogGroupName: "${local.project}-InstanceConfiguration"
+        CloudWatchOutputEnabled: true
   - name: "SendExecutionLog"
-    action: "aws:executeAutomation"
+    action: "aws:executeAwsApi"
+    isEnd: true
     inputs:
-      DocumentName: "SendExecutionLog"
-      RuntimeParameters:
-        EventSource:
-        - {{ EventSource }}
+      Service: "sns"
+      Api: "Publish"
+      TopicArn: "${aws_sns_topic.default.arn}"
+      Subject: "Instance configuration for ${local.project}"
+      Message: "Instance {{ ExtractInstanceName.InstanceName }} {{ FilterInstancesByNameTag.InstanceIds }} configuration {{ automation:EXECUTION_ID }} completed at {{ global:DATE_TIME }}"
 EOF
 }
