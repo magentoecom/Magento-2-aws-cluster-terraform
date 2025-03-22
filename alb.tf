1af7781082e62cb407528fe60e81383dc8602e6f
MagenX <magenx@users.noreply.github.com>
2025-03-22 17:49:30 +0100

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -7,12 +7,12 @@
 # Create Application Load Balancers
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_lb" "this" {
-  name               = "${local.project}-${each.key}-alb"
+  name               = "${local.project}-alb"
   internal           = false
   load_balancer_type = "application"
-  drop_invalid_header_fields = true
   security_groups    = [aws_security_group.alb.id]
-  subnets            = [for az, subnet in aws_subnet.this : subnet.id][0:2]
+  subnets            = [for subnet in aws_subnet.this : subnet.id]
+  drop_invalid_header_fields = true
   access_logs {
     bucket  = aws_s3_bucket.this["system"].bucket
     prefix  = "ALB"
@@ -47,32 +47,16 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.this.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
   certificate_arn   = aws_acm_certificate.default.arn
   default_action {
     type             = "fixed-response"
     fixed_response {
         content_type = "text/plain"
         message_body = "No targets are responding to this request"
-        status_code  = "502"
+        status_code  = "418"
         }
     }
-}
-# # ---------------------------------------------------------------------------------------------------------------------#
-# Create conditional listener rule for Load Balancer - forward to varnish
-# # ---------------------------------------------------------------------------------------------------------------------#
-resource "aws_lb_listener_rule" "varnish" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 30
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
-  }
-  condition {
-    host_header {
-      values = [var.domain]
-    }
-  }
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create http:// listener for Load Balancer - redirect to https://
@@ -87,6 +71,22 @@ resource "aws_lb_listener" "http" {
       port        = "443"
       protocol    = "HTTPS"
       status_code = "HTTP_301"
+    }
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create conditional listener rule for Load Balancer - forward to varnish
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "aws_lb_listener_rule" "varnish" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 30
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+  condition {
+    host_header {
+      values = [var.domain]
     }
   }
 }
