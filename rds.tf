1970ea86f3acab04b0616c97dac35b028cd4aa3c
MagenX <magenx@users.noreply.github.com>
2025-04-01 19:28:16 +0200

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -47,7 +47,7 @@ resource "aws_db_instance" "this" {
   multi_az               = var.rds["multi_az"]
   db_name                = local.db_name
   username               = var.brand
-  password               = random_password.this["rds"].result
+  password               = random_password.this["database"].result
   parameter_group_name   = aws_db_parameter_group.this.id
   skip_final_snapshot    = var.rds["skip_final_snapshot"]
   vpc_security_group_ids = [aws_security_group.rds.id]
